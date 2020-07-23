2cbceb56242fbe089fa6fe9b0dc989f5c1445361
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-23 09:35:01 +0200

    remove unused lines

--- Notebook/Reactjs/learning-hooks/src/useWindowDimensions.js
+++ Notebook/Reactjs/learning-hooks/src/useWindowDimensions.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const useWindowDimensions = () => {
   const [innerWidth, setInnerWidth] = useState(window.innerHeight);
